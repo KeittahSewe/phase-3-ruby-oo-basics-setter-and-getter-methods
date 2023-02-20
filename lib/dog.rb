e38d3d8a049224f3f6d2d93ff538074add30fb14
Keittah Sewe <kseweo@gmail.com>
2023-02-20 10:54:42 +0300

    corrections

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,3 +15,23 @@ def name=(name)
         @breed
     end   
 end
+
+class Person
+
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end    
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end    
+end
